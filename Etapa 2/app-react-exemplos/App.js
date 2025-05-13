b7aefddf6dbb9b2d5e0a315b720391f55bc689a9
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2025-05-13 08:12:11 -0300

    View basic examples

--- Etapa 2/app-react-exemplos/App.js
+++ Etapa 2/app-react-exemplos/App.js
@@ -8,7 +8,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <List/>
+      <List />
+      <View style={styles.redbox}></View>
+      <View style={styles.greenbox}></View>
+      <View style={styles.bluebox}></View>
     </View>
   );
 }
@@ -17,57 +20,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    marginTop: 50
-  },
-
-  title: {
-    fontSize: 28,
-    marginBottom: 20,
-  },
-
-  buttonContainer: {
-    flexDirection: 'row',
-    gap: 5,
-  },
-
-  input: {
-    height: 40,
-    borderBottomColor: '#8706d4',
-    borderBottomWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-
-  list: {
-    marginTop: 20,
-    overflow:  'scroll',
-    height: 'auto',
-  },
-
-  item: {
-    flexDirection: 'row',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-    padding: 10,
-    backgroundColor: '#f0f2ff',
-    borderRadius: 5,
-    gap: 5,
- },
- itemText: {
-  flex: 1,
-  marginRight: 10,
- },
- button: {
-  flexDirection: 'row',
-  gap: 5,
- },
- editInput: {
-  flex: 1,
-  height: 40,
-  borderBottomColor: '#8706d4',
-  borderBottomWidth: 1,
-  marginBottom: 10,
-  paddingHorizontal: 10,
-}
+    backgroundColor: 'aliceblue',
+    height: '100%',
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    borderRadius: 8,
+  },
+  greenbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+    borderRadius: 8,
+  },
+  bluebox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+  },
 });
