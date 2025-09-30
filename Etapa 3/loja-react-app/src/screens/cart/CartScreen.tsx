e3d0d239c4d543da8881ee85ff65e4f2b4596b21
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2025-09-30 09:26:47 -0300

    Estilização do Carrinho - Parcial

--- Etapa 3/loja-react-app/src/screens/cart/CartScreen.tsx
+++ Etapa 3/loja-react-app/src/screens/cart/CartScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
 import CartItem from './CartItem';
 
@@ -8,29 +8,115 @@ import { useShop } from '../../contexts/ShopContext';
 const CartScreen = ({ navigation }: any) => {
     const { cartItems } = useShop();
 
-    const renderItem = ({item} : any) => (
+    const renderItem = ({ item }: any) => (
         <CartItem item={item} />
     );
 
-    const cartTemp = [
-        { 
-            id: 1,
-            quantity: 2,
-            price: 8.5,
-            name: "Brigadeiro promoção especial",
-            image: "http://10.81.205.50:5000/uploads/brigadeiro-matcha-white.png",
-        }
-    ];
+    const handleCheckout = () => {
+        console.log('Concluindo a compra')
+    }
 
     return (
-        <View> 
-            <Text>Carrinho de compras</Text>
-            <FlatList 
-                data={cartItems}
-                renderItem={renderItem}
-                keyExtractor={(item: any) => item.id.toString()}
-            />
+        <View style={styles.container}>
+            {cartItems.length === 0 ? (
+                <View>
+                    <Text style={styles.empty}>Seu Carrinho está vazio!</Text>
+                    <Button title='Voltar para o Menu' onPress={() => navigation.navigate('Catalog')} />
+                </View>
+            ) : (
+                <>
+                    <View>
+                        <Text>Carrinho de compras</Text>
+                        <FlatList
+                            data={cartItems}
+                            renderItem={renderItem}
+                            keyExtractor={(item: any) => item.id.toString()}
+                        />
+                    </View>
+                    <View style={styles.totalContainer}>
+                        <Text style={styles.totalText}>Total R$ {handleCheckout}</Text>
+                        <TouchableOpacity
+                            onPress={handleCheckout}
+                            style={styles.clearBtn}
+                        >
+                            <Text style={styles.clearBtnTxt}>Limpar Carrinho</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Catalog')}
+                            style={styles.continueBtn}
+                        >
+                            <Text style={styles.continueBtnTxt}>Continuar comprando</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Catalog')}
+                            style={styles.checkoutBtn}
+                        >
+                            <Text style={styles.checkoutBtnTxt}>Concluir pedido</Text>
+                        </TouchableOpacity>
+                    </View>
+                </>
+            )
+            }
         </View>
     );
 };
 export default CartScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 150,
+    },
+    empty: {
+        fontSize: 16,
+        marginBottom: 20,
+    },
+    listContainer: {
+        flex: 1,
+    },
+    totalContainer: {
+        padding: 10,
+        borderWidth: 1,
+        backgroundColor: '#F9F9F9',
+        borderTopColor: '#CCC',
+    },
+    totalText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    clearBtn: {
+        marginTop: 10,
+        backgroundColor: 'grey',
+        padding: 10,
+        borderRadius: 5,
+    },
+    clearBtnTxt: {
+        color: '#FFF',
+        textAlign: 'center',
+    },
+    continueBtn: {
+        marginTop: 10,
+        backgroundColor: '#007BFF',
+        padding: 10,
+        borderRadius: 5,
+    },
+    continueBtnTxt: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 16,
+    },
+    checkoutBtn: {
+        marginTop: 10,
+        backgroundColor: '#28A745',
+        padding: 10,
+        borderRadius: 5,
+    },
+    checkoutBtnTxt: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 16,
+    }
+})
